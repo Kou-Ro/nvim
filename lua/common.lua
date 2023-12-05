d5c7205c38101891ac23fff910c433319c4da986
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2023-12-05 23:55:58 +0900

    add module setting

--- lua/common.lua
+++ lua/common.lua
@@ -1,3 +1,8 @@
+-- Functions definition
 require "function"
 
+-- Vim options settings
 require "vimopt"
+
+-- Package manager
+vset("runtimepath", vget("runtimepath") .. "," .. NDOTDIR .. "module")
--- lua/function.lua
+++ lua/function.lua
@@ -3,13 +3,13 @@
 -- @param value Option value
 -- @return Value after set
 function vset(variableName, value)
-    vim.opt[variableName] = value
-    return vim.opt[variableName]:get()
+    vim.o[variableName] = value
+    return vim.o[variableName]
 end
 
 --- Get vim options
 -- @param variableName Option variableName
 -- @return Option value
 function vget(variableName)
-    return vim.opt[variableName]:get()
+    return vim.o[variableName]:get()
 end
