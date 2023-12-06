ff85ffefdd294a01b73e4ad1c973ea91018c73f7
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2023-12-06 15:03:33 +0900

    add opt

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,8 +4,8 @@ vset("runtimepath", vget("runtimepath") .. "," .. NDOTDIR .. "/module/packer.nvi
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 return require('packer').startup(function(use)
     -- Packer can manage itself
-    use {"wbthomason/packer.nvim"}
+    use({ "wbthomason/packer.nvim" })
 
     -- Color scheme
-    use {"folke/tokyonight.nvim"}
+    use({ "folke/tokyonight.nvim", opt = true })
 end)
