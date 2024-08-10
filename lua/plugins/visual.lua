34d0201dde3dadd957480963983bead67d2f7c21
KouRo <87910604+Kou-Ro@users.noreply.github.com>
2024-08-10 23:24:01 +0900

    LastOfDirSet

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -7,6 +7,10 @@ return {
           "nvim-tree/nvim-web-devicons"
       },
     },
+    config = function()
+        require("dirSet")
+        require("luahoge")
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
