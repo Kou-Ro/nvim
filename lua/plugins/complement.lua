7a9c9ef806fc59c70a48ed51e0cc66d866e766c9
KouRo <git@kouro.dev>
2024-12-14 23:31:49 +0900

    copilot5

--- lua/plugins/complement.lua
+++ lua/plugins/complement.lua
@@ -21,5 +21,22 @@ return {
     },
     {
         "hrsh7th/nvim-cmp",
-    }
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        event = {"InsertEnter", "CmdlineEnter"},
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "copilot" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+            })
+        end
+    },
 }
