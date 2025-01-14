23759a667842cf1e7acfa0f9fd86597ef58f5dc8
KouRo <git@kouro.dev>
2025-01-15 02:49:00 +0900

    Custum lualine

--- lua/pluginConfig/lualine.lua
+++ lua/pluginConfig/lualine.lua
@@ -1,19 +1,35 @@
 require('lualine').setup{
   tabline = {
-    lualine_a = {},
-    lualine_b = {'branch'},
+    lualine_a = {'branch'},
+    lualine_b = {'filetype', 'encoding'},
     lualine_c = {'filename'},
-    lualine_x = {},
+    lualine_x = {{
+      'buffers',
+      show_filename_only = true,
+      hide_filename_extension = false,
+      show_modified_status = false,
+      mode =  4,
+    }},
     lualine_y = {},
     lualine_z = {}
   },
   sections = {
-    lualine_b = {'branch','diff'},
+    lualine_a = {'mode'},
+    lualine_b = {'diff'},
     lualine_c = {{
       'diagnostics',
       sources = {'nvim_lsp'},
       symbols = {error = '✕', warn = '⚠', info = '⭘', hint = '✓'},
     }},
     lualine_x = {function() return '#'..tostring(vim.api.nvim_get_current_buf());end},
+    lualine_y = {'progress',{
+      'location',
+      fmt = function(disp)return string.gsub(disp,':','-')end
+    }},
+    lualine_z = {
+      {
+        'datetime',
+        style = '%H:%M:%S',
+      }},
+    }
   }
-}
