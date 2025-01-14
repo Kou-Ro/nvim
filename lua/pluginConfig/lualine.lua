db78f1fcd0d616ad6a52add5cc6524b7b8660949
KouRo <git@kouro.dev>
2025-01-15 01:37:43 +0900

    Show lsp progress and active all plugins

--- lua/pluginConfig/lualine.lua
+++ lua/pluginConfig/lualine.lua
@@ -1,14 +1,19 @@
 require('lualine').setup{
-    tabline = {
-        lualine_a = {},
-        lualine_b = {'branch'},
-        lualine_c = {'filename'},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {}
-    },
-    sections = {
-        lualine_c = {'lsp_progress'},
-        lualine_x = {function() return '#'..tostring(vim.api.nvim_get_current_buf());end},
-    }
+  tabline = {
+    lualine_a = {},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  sections = {
+    lualine_b = {'branch','diff'},
+    lualine_c = {{
+      'diagnostics',
+      sources = {'nvim_lsp'},
+      symbols = {error = '✕', warn = '⚠', info = '⭘', hint = '✓'},
+    }},
+    lualine_x = {function() return '#'..tostring(vim.api.nvim_get_current_buf());end},
+  }
 }
