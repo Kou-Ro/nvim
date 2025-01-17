fe8094b7a42079a07353f23e5fbb8950ffb711cb
KouRo <git@kouro.dev>
2025-01-17 14:26:48 +0900

    Format

--- lua/pluginConfig/nvim-cmp.lua
+++ lua/pluginConfig/nvim-cmp.lua
@@ -1,38 +1,38 @@
 local cmp = require('cmp')
-        cmp.setup({
-          snippet = {
-            expand = function(args)
-              require('luasnip').lsp_expand(args.body)
-            end,
-          },
-          view = {
-            entries = {
-              name = 'custom',
-              selection_order = 'near_cursor',
-            },
-          },
-          window = {
-            completion = {
-              max_height = 10,
-            }
-          },
-          formatting = {
-            format = require('lspkind').cmp_format()
-          },
-          mapping = cmp.mapping.preset.insert({
-            ['<C-j>'] = cmp.mapping.select_next_item(),
-            ['<C-k>'] = cmp.mapping.select_prev_item(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            ['<C-q>'] = cmp.mapping.abort(),
-            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-c>'] = cmp.mapping.complete(),
-          }),
-          sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-            { name = 'buffer' },
-            { name = 'path' },
-            { name = 'copilot' }
-          }),
-        })
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  view = {
+    entries = {
+      name = 'custom',
+      selection_order = 'near_cursor',
+    },
+  },
+  window = {
+    completion = {
+      max_height = 10,
+    },
+  },
+  formatting = {
+    format = require('lspkind').cmp_format(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-q>'] = cmp.mapping.abort(),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-c>'] = cmp.mapping.complete(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'copilot' },
+  }),
+})
