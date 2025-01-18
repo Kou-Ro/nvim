66c6e8eb7d5d137fbf97fe025eb30561e0e4cd5c
KouRo <git@kouro.dev>
2025-01-19 01:40:09 +0900

    Set lua format

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,8 +1,16 @@
 local opt_local = vim.opt_local
+local bo = vim.bo
 
 vim.api.nvim_create_autocmd({ 'TermOpen' }, {
   callback = function(arg)
     opt_local.number = false
     opt_local.signcolumn = 'no'
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  callback = function(arg)
+    bo.shiftwidth = 2
+    bo.tabstop = 2
   end,
 })
