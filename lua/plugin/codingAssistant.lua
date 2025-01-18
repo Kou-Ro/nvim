ed4d45b2a3d802bec4b956f0b1fa47caaa2e868f
KouRo <git@kouro.dev>
2025-01-19 01:57:34 +0900

    enable treesitter

--- lua/plugin/codingAssistant.lua
+++ lua/plugin/codingAssistant.lua
@@ -30,6 +30,7 @@ if enabled then
     },
     {
       'windwp/nvim-autopairs',
+      enabled = true,
       event = 'InsertEnter',
       config = function()
         local autopairs = require('nvim-autopairs')
@@ -42,58 +43,11 @@ if enabled then
     },
     {
       'nvim-treesitter/nvim-treesitter',
-      enabled = false,
+      enabled = true,
       lazy = true,
       build = ':TSUpdate',
       config = function()
-        local configs = require('nvim-treesitter.configs')
-        configs.setup({
-          ensure_installed = {
-            'arduino',
-            'bash',
-            'c',
-            'c_sharp',
-            'cmake',
-            'comment',
-            'cpp',
-            'css',
-            'diff',
-            'dockerfile',
-            'git_config',
-            'git_rebase',
-            'gitattributes',
-            'gitcommit',
-            'gitignore',
-            'java',
-            'javascript',
-            'jsdoc',
-            'json',
-            'json5',
-            'jsonc',
-            'kotlin',
-            'latex',
-            'lua',
-            'make',
-            'markdown',
-            'markdown_inline',
-            'passwd',
-            'python',
-            'rust',
-            'scheme',
-            'sql',
-            'ssh_config',
-            'tsx',
-            'typescript',
-            'udev',
-            'vim',
-            'vimdoc',
-            'yaml',
-          },
-          sync_install = false,
-          auto_install = true,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
+        require('pluginConfig/nvim-treesitter')
       end,
     },
   }
