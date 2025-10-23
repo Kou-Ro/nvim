69056278a782c38758433bdb55f3a2dc3dd69953
KouRo <git@kouro.dev>
2025-10-23 11:44:53 +0900

    Remove lsp

--- lua/pluginConfig/mason-lspconfig-servers.lua
+++ lua/pluginConfig/mason-lspconfig-servers.lua
@@ -1,5 +1,4 @@
 return {
-  'api-linter', -- A linter for API
   'arduino_language_server', -- Arduino
   'asm_lsp', -- Assembly
   'ast_grep',
@@ -9,18 +8,13 @@ return {
   'clangd', -- C/C++
   'cmake', -- CMakej
   'codebook',
-  'css-lsp',
   'dockerls', -- Docker
   'docker_compose_language_service', -- Docker
-  'dotenv-linter', -- .env
   'eslint', -- JavaScript
   'harper_ls',
-  'html-lsp', -- HTML
   'kotlin_language_server', -- Kotlin
   'ltex', -- LaTex
   'lua_ls', -- Lua
-  'pgformatter', -- PostgreSQL
-  'prisma-language-server', -- Prisma
   'rust_analyzer', -- Rust
   'ts_ls', -- TypeScript
   'vimls', -- VimScript
