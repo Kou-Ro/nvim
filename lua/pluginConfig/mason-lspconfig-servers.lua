c522cb6bb0873fecd5cd03544603d3192ebaac67
KouRo <git@kouro.dev>
2025-10-27 00:38:31 +0900

    Rm all lsp

--- lua/pluginConfig/mason-lspconfig-servers.lua
+++ lua/pluginConfig/mason-lspconfig-servers.lua
@@ -1,22 +1,22 @@
 return {
-  'arduino_language_server', -- Arduino
-  'asm_lsp', -- Assembly
-  'ast_grep',
-  'autotools_ls',
-  'bashls', -- Bash
-  'biome', -- JavaScript
-  'clangd', -- C/C++
-  'cmake', -- CMakej
-  'codebook',
-  'dockerls', -- Docker
-  'docker_compose_language_service', -- Docker
-  'eslint', -- JavaScript
-  'harper_ls',
-  'kotlin_language_server', -- Kotlin
-  'ltex', -- LaTex
-  'lua_ls', -- Lua
-  'prisma-language-server', -- Prisma
-  'rust_analyzer', -- Rust
-  'ts_ls', -- TypeScript
-  'vimls', -- VimScript
+  --  'arduino_language_server', -- Arduino
+  --  'asm_lsp', -- Assembly
+  --  'ast_grep',
+  --  'autotools_ls',
+  --  'bashls', -- Bash
+  --  'biome', -- JavaScript
+  --  'clangd', -- C/C++
+  --  'cmake', -- CMakej
+  --  'codebook',
+  --  'dockerls', -- Docker
+  --  'docker_compose_language_service', -- Docker
+  --  'eslint', -- JavaScript
+  --  'harper_ls',
+  --  'kotlin_language_server', -- Kotlin
+  --  'ltex', -- LaTex
+  --  'lua_ls', -- Lua
+  --  'prisma-language-server', -- Prisma
+  --  'rust_analyzer', -- Rust
+  --  'ts_ls', -- TypeScript
+  --  'vimls', -- VimScript
 }
