893d7b674b5f1e6051fff690591af08b5b9a310e
KouRo <git@kouro.dev>
2025-12-19 02:04:14 +0900

    NextJS indent

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -15,3 +15,40 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
     bo.tabstop = 2
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    'javascript',
+    'javascriptreact',
+    'jsx',
+    'typescript',
+    'typescriptreact',
+    'tsx',
+    'json',
+    'jsonc',
+    'css',
+    'scss',
+    'html',
+    'yaml',
+    'markdown',
+  },
+  callback = function()
+    bo.shiftwidth = 2
+    bo.tabstop = 2
+    bo.softtabstop = 2
+    bo.expandtab = true
+    bo.autoindent = true
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    'javascriptreact',
+    'jsx',
+    'typescriptreact',
+    'tsx',
+  },
+  callback = function()
+    bo.identexpr = ''
+  end,
+})
