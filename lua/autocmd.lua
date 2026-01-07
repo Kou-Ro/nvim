3a12bf676ab807b09bbe10b7db09292e77e1581d
KouRo <git@kouro.dev>
2026-01-08 03:07:28 +0900

    nvim-treesitter autocmd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -75,3 +75,12 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     })
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    vim.treesitter.start()
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
