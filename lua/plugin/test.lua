dbd583bf32c35303d4962bdc3772c14bb56ebe92
KouRo <git@kouro.dev>
2026-01-26 04:44:59 +0900

    vitest debug

--- lua/plugin/test.lua
+++ lua/plugin/test.lua
@@ -10,6 +10,7 @@ if enabled then
         'nvim-lua/plenary.nvim',
         'antoinemadec/FixCursorHold.nvim',
         'nvim-treesitter/nvim-treesitter',
+        'marilari88/neotest-vitest',
       },
       config = function()
         require('pluginConfig/neotest')
@@ -21,6 +22,10 @@ if enabled then
       dependencies = {
         'nvim-telescope/telescope.nvim',
       },
+    },
+    {
+      'marilari88/neotest-vitest',
+      enabled = false,
     },
   }
 else
