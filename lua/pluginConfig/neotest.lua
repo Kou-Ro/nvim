46fa15a563a17a0f2f9e6a463552018d7a7e586c
KouRo <git@kouro.dev>
2026-01-26 04:37:32 +0900

    vitest map

--- lua/pluginConfig/neotest.lua
+++ lua/pluginConfig/neotest.lua
@@ -12,26 +12,31 @@ require('neotest').setup({
       },
     }),
   },
-})
-(function()
+})(function()
   local keymap = require('utils/keymap')
   local set = keymap.set
   local n = keymap.mode.NORMAL
 
   local neotest = require('neotest')
 
-  -- neotestのキーマッピング
+  -- For all neotest adapters
   set(n, '<leader>tt', neotest.run.run(), 'Run nearest test')
   set(n, '<leader>tf', neotest.run.run(vim.fn.expand('%')), 'Run current file tests')
   set(n, '<leader>td', neotest.run.run({ strategy = 'dap' }), 'Debug nearest test')
   set(n, '<leader>ts', neotest.summary.toggle(), 'Toggle test summary')
   set(n, '<leader>to', neotest.output.open({ enter = true }), 'Open test output')
 
-  -- neotest-playwright専用のコマンド群
+  -- For neotest-playwright
   set(n, '<leader>tp', ':NeotestPlaywrightProject<CR>', 'Playwright project settings')
   set(n, '<leader>tpr', ':NeotestPlaywrightPreset<CR>', 'Playwright preset')
   set(n, '<leader>tra', ':NeotestPlaywrightRefresh<CR>', 'Playwright refresh')
-
-  -- プリセットをheadedに設定（ブラウザ表示）
   set(n, '<leader>th', ':NeotestPlaywrightPreset<CR>', 'Set headed preset')
+
+  set(n, '<leader>twr', require('neotest').run.run({ vitestCommand = 'vitest--watch' }), 'RunWatch')
+  set(
+    n,
+    '<leader>twf',
+    require('neotest').run.run({ vim.fn.expand('%'), vitestCommand = 'vitest--watch' }),
+    'RunWatchFile'
+  )
 end)()
