aacc6f39288aa5c299c84bb31eed1b5bd7e92ef0
KouRo <git@kouro.dev>
2026-08-15 08:20:42 +0900

    feat: optimize for VS Code Neovim and WSL2 clipboard integration

--- init.lua
+++ init.lua
@@ -3,4 +3,11 @@ if os.getenv('DOTFILES') then
   NDOTDIR = os.getenv('DOTFILES') .. '/nvim'
 end
 
+-- VS Code Neovim environment
+if vim.g.vscode then
+  require('function')
+  require('vimopt')
+  return
+end
+
 require('common')
--- lua/vimopt.lua
+++ lua/vimopt.lua
@@ -11,3 +11,19 @@ opt.hls = false
 opt.cursorline = true
 opt.signcolumn = 'yes:1'
 opt.clipboard = 'unnamedplus'
+
+-- WSL2 clipboard integration
+if vim.fn.has('wsl') == 1 then
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
